b117c54d69d048122765a527a5b039e4cc07c44f
touthecode <130022845+touthecode@users.noreply.github.com>
2023-05-18 22:37:14 +0800

    added integer text file and main program guide

---
+++ odd and even segregator.py
@@ -0,0 +1,4 @@
+# The program should open a file named "integer.txt" which contains 20 integers
+# The program will then read the text file then create 2 text files namely "odd.txt" and "even.txt"
+# "odd.txt" will then contain odd numbers while "even.txt" shall contain even numbers both coming from the numbers in "integer.txt"
+
