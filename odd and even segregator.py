4a847f1d4caa7b7b0be3e228cc3fc5440f8700f3
touthecode <130022845+touthecode@users.noreply.github.com>
2023-05-25 16:13:13 +0800

    code is now working, inverted slashes

--- odd and even segregator.py
+++ odd and even segregator.py
@@ -6,13 +6,12 @@
 import sys
 print(sys.path)
 
-with open("C:\Users\Tou\Desktop\School\OOP\Python Stuff\Odd and Even\integer.txt") as int_file:
+with open("C:/Users/Tou/Desktop/School/OOP/Python Stuff/Odd and Even/integer.txt") as int_file:
     numbers = int_file.readlines()
 
-with open("C:\Users\Tou\Desktop\School\OOP\Python Stuff\Odd and Even\odd.txt") as output_odd, open("C:\Users\Tou\Desktop\School\OOP\Python Stuff\Odd and Even\even.txt") as output_even:
+with open("C:/Users/Tou/Desktop/School/OOP/Python Stuff/Odd and Even/odd.txt", "w") as output_odd, open("C:/Users/Tou/Desktop/School/OOP/Python Stuff/Odd and Even/even.txt", "w") as output_even:
     for line in numbers:
         int_line = int(line)
-
         #Identify whether the numbers are odd or even then put the number in their respective text files
         if int_line %2 == 0:
             output_even.write(str(int_line) + "\n")
